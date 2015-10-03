f1290dfafa7d10736af951b0c7d9d67f26f38b4e
Tieshun Roquerre <tieshun.roquerre@gmail.com>
2015-10-03 15:35:07 -0700

    Create db package

---
+++ db.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+var (
+	db   gorm.DB
+	once sync.Once
+)
+
+func connect() {
+	var err error
+	db, err = gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		log.Panic("Failed to open database connection to postgres: ", err)
+	}
+
+	err = db.DB().Ping()
+
+	if err != nil {
+		log.Panic("Dead connection to postgres: ", err)
+	}
+
+	if maxConnections := os.Getenv("DATABASE_MAX_CONNECTIONS"); maxConnections == "" {
+		db.DB().SetMaxOpenConns(100)
+	} else if mc, err := strconv.Atoi(maxConnections); err == nil {
+		db.DB().SetMaxOpenConns(mc)
+	} else {
+		log.Panic("Could not convert DATABASE_MAX_CONNECTIONS to int: ", err)
+	}
+}
+
+func DB() gorm.DB {
+	once.Do(connect)
+	return db
+}
